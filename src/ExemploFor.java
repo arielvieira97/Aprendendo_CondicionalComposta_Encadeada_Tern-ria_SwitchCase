06fa56436b9981bce98a123ea8eab8b2c1fcc442
Ariel Vieira <arielvieiragalvao@hotmail.com>
2024-05-26 13:50:36 -0300

    atualização

---
+++ src/ExemploFor.java
@@ -0,0 +1,7 @@
+public class ExemploFor{
+    public static void main(String[] args) {
+        for (int Carneirinhos = 1; Carneirinhos <= 20; Carneirinhos ++){
+            System.out.println("Contando Carneirinhos" + Carneirinhos);
+        }
+    }
+}
